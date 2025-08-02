81c58967f0bfe6b5c35c3c3ade770a36c4668b41
DevByDipto <dipto.1900@gmail.com>
2025-08-02 11:25:43 +0600

    n

---
+++ next/serverside-rendering.js
@@ -0,0 +1,11 @@
+{/*
+   next js serverside render o rerender:
+
+   ->normally first time page load howar somoi ja server side component ta server side theke render hoi akdom html ta ase abong ta google serch bot read kore index kore
+   -> ar jokhon amra manually kono route e hit kori jmn route.push() tokhon oi route e jaa jaa server side functanality thakbe ta server theke rerender ba rendder hoi asbe aikhetre googl bot ar html ta read korbenah karon google bot tokhon e html read kore jokhon kono link e se nije theke jai .ar se kono link e nije theke tokhon jai jokhon <Link herf="...." aivabe kono link thake. 
+   tobe রানটাইমে rerender হওয়া HTML Google bot এর চোখে পড়ে না, কারণ Google bot:
+   শুধু একবার HTTP request করে
+   তারপর response হিসাবে যেটা server পাঠায় — সেটাই দেখে
+
+    
+    */}
